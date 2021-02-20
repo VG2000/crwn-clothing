ce80d4011152fb257e3b4db58a50321d594f1708
Vincent Gomez <vincentgomez@vincents-imac.home>
2021-02-20 08:47:33 +0000

    implement code that pulls in shop data from firestore

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import {  setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors'
-
+ 
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -58,7 +58,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,42 +11,72 @@ const config = {
     appId: "1:429706259987:web:14767d6b4bbc0721b999e6",
     measurementId: "G-LTZ2DQRB8C"
   };
+  firebase.initializeApp(config);
 
   export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
-
-
+  
     const userRef = firestore.doc(`users/${userAuth.uid}`);
-    
+  
     const snapShot = await userRef.get();
-
-    if(!snapShot.exists) {
+  
+    if (!snapShot.exists) {
       const { displayName, email } = userAuth;
       const createdAt = new Date();
-
       try {
         await userRef.set({
           displayName,
           email,
           createdAt,
           ...additionalData
-        })
+        });
       } catch (error) {
-        console.log('error creating user', error.message)
+        console.log('error creating user', error.message);
       }
-
     }
+  
     return userRef;
-
-  }
-
-  firebase.initializeApp(config)
-
+  };
+  
+  export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+  ) => {
+    const collectionRef = firestore.collection(collectionKey);
+  
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, obj);
+    });
+  
+    return await batch.commit();
+  };
+  
+  export const convertCollectionsSnapshotToMap = collections => {
+    const transformedCollection = collections.docs.map(doc => {
+      const { title, items } = doc.data();
+  
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items
+      };
+    });
+  
+    return transformedCollection.reduce((accumulator, collection) => {
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    }, {});
+  };
+  
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
-
+  
   const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({ prompt: 'select_account' });
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
+  
   export default firebase;
+  
